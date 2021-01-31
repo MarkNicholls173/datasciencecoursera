6678429e8692ecdb3452da6cb096272fe1cdce6b
Mark Nicholls <mark.nicholls@dvsa.gov.uk>
2021-01-31 15:49:16 +0000

    finish rankall fuction

--- rankall.R
+++ rankall.R
@@ -1,11 +1,11 @@
-rankhospital <- function(state, outcome, num = "best"){
+rankall <- function(outcome, num = "best"){
         ## state:       character abbrevation for state
-        ## outcome:     one of "heart attack", "heart failuer" or "pnuemonia"
         ## num:          ranking of hospital in that outcome (best|worse or int rank)
         ##
-        ## return character vector with name of hospital
+        ## return 2 columns dataframe containing 1 row for each state
+        ##     Hospital Name | state
         ##
-        ## if num is larger than the number of hospitals in that state return NA
+        ## return NA if if no rank exists in that state
         ##
         ## columns of interest
         ## [2] Hospital.Name
@@ -15,9 +15,8 @@ rankhospital <- function(state, outcome, num = "best"){
         ## [23] pnuemonia mortality
         ##
         ## validate inputs state is valid and outcome is one of the 3
-        ## stop "invalid state|outcome"
+        ## stop "invalid state|outcome/num"
         ##
-        ## ties: return all results alphabetically
         
         ##read outcome data
         allOutcomeData <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
@@ -25,112 +24,126 @@ rankhospital <- function(state, outcome, num = "best"){
         ## get distinct list of states
         states = sort(unique(allOutcomeData[,7]))
         
-
-        ## test to see if state is in list of states
-        ## stop if match not found
-        if(!any(states == state)){ stop("invalid state") }
-        
-        
         ## set outcome column and test for invalid outcome
         if(outcome == "heart attack"){
                 outcomeCol <-  11
         } else if(outcome == "heart failure"){
                 outcomeCol <- 17
-        } else if(outcome == "pnuemonia") {
+        } else if(outcome == "pneumonia") {
                 outcomeCol <- 23
         } else {
                 stop("invalid outcome")
         }
         
-        ##subset for state, get name and outcome result
-        myOutcomeData <- allOutcomeData[allOutcomeData[7] == state, c(2, outcomeCol)]
-        ##set outcome to numeric
-        myOutcomeData[,2] <- as.numeric(myOutcomeData[,2]) 
         
-        ##filter for NA
-        myOutcomeData <- myOutcomeData[!is.na(myOutcomeData[2]), ]
+        
+        ##set outcome to numeric
+        allOutcomeData[, outcomeCol] <- as.numeric(allOutcomeData[,outcomeCol])
+        
+        ##subset for not NA, get name, state and outcome
+        myOutcomeData <- allOutcomeData[!is.na(allOutcomeData[outcomeCol]), c(2, 7, outcomeCol)]
         
         ##user order to sort DF by col 2 then col 1
-        myOutcomeData <- myOutcomeData[order(myOutcomeData[,2], myOutcomeData[, 1]),]
+        myOutcomeData <- myOutcomeData[order(myOutcomeData[,2], myOutcomeData[,3], myOutcomeData[,1]),]
+        
+        ##split data by state
+        statetables <- split(myOutcomeData, myOutcomeData[2])
         
         #resolve best|worse|int for rank
         if(!is.numeric(num)){
                 if(num == "best"){
                         num <-  1
                 } else if(num == "worst") {
-                        num <- nrow(myOutcomeData)
+                        worstFlag <- TRUE
                 } else {
                         stop("invalid rank passed")
                 }
                 
         }
         
+        #make results dataframe
+        results <- data.frame(states)
+        rownames(results) <- states
+        for(i in seq_along(statetables)) {
+                if(worstFlag){ num <- nrow(statetables[[i]])}
+                results[i,1:2] <- statetables[[i]][num,1:2]
+        }
+        
+        colnames(results) <- c("hospital", "state")
+        
+        
         ##return result
-        myOutcomeData[num, 1]
+        results
         
 }
 
 
-state <-  "TX"
-#state <-  "BB"
+
 #outcome <- "heart attack"
-outcome <- "heart failure"
-#outcome <- "pnuemonia"
+#outcome <- "heart failure"
+outcome <- "pneumonia"
 #outcome <- "invalid"
-num = 4
+#num = 20
 #num <- "best"
-#num <- "worst"
+num <- "worst"
 #num <- "wrong"
 
 
 ##read outcome data
 allOutcomeData <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-#outcome[, 11] <-  as.numeric(outcome[,11])
 
 ## get distinct list of states
 states = sort(unique(allOutcomeData[,7]))
-
-## test to see if state is in list of states
-# stop if match not found
-if(!any(states == state)){ stop("invalid state") }
-
 
 ## set outcome column and test for invalid outcome
 if(outcome == "heart attack"){
         outcomeCol <-  11
 } else if(outcome == "heart failure"){
         outcomeCol <- 17
-} else if(outcome == "pnuemonia") {
+} else if(outcome == "pneumonia") {
         outcomeCol <- 23
 } else {
         stop("invalid outcome")
 }
 
 
-##subset for state, get name and outcome result
-myOutcomeData <- allOutcomeData[allOutcomeData[7] == state, c(2, outcomeCol)]
-##set outcome to numeric
-myOutcomeData[,2] <- as.numeric(myOutcomeData[,2]) 
 
-##filter for NA
-myOutcomeData <- myOutcomeData[!is.na(myOutcomeData[2]), ]
+##set outcome to numeric
+allOutcomeData[, outcomeCol] <- as.numeric(allOutcomeData[,outcomeCol])
+
+##subset for not NA, get name, state and outcome
+myOutcomeData <- allOutcomeData[!is.na(allOutcomeData[outcomeCol]), c(2, 7, outcomeCol)]
 
 ##user order to sort DF by col 2 then col 1
-myOutcomeData <- myOutcomeData[order(myOutcomeData[,2], myOutcomeData[, 1]),]
+myOutcomeData <- myOutcomeData[order(myOutcomeData[,2], myOutcomeData[,3], myOutcomeData[,1]),]
+
+##split data by state
+statetables <- split(myOutcomeData, myOutcomeData[2])
 
 #resolve best|worse|int for rank
 if(!is.numeric(num)){
         if(num == "best"){
                 num <-  1
         } else if(num == "worst") {
-                num <- nrow(myOutcomeData)
+                worstFlag <- TRUE
         } else {
                 stop("invalid rank passed")
         }
                 
 }
 
+#make results dataframe
+results <- data.frame(states)
+rownames(results) <- states
+for(i in seq_along(statetables)) {
+        if(worstFlag){ num <- nrow(statetables[[i]])}
+        results[i,1:2] <- statetables[[i]][num,1:2]
+}
+
+colnames(results) <- c("hospital", "state")
+
 ##return result
-myOutcomeData[num, 1]
+head(results, 10)
+tail(results, 3)
 
 
