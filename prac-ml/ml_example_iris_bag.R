b107c39a5f3060de8fa2b0580bb3d8562d2b48e6
Mark Nicholls <mark.nicholls@dvsa.gov.uk>
2021-05-25 16:05:03 +0100

    bag example

---
+++ prac-ml/ml_example_iris_bag.R
@@ -0,0 +1,30 @@
+#libraries
+library(caret)
+library(ggplot2)
+library(rpart)
+
+#load data & basic info
+data(iris)
+names(iris)
+table(iris$Species)
+
+#split data & basic info
+inTrain <- createDataPartition(y=iris$Species,
+                               p=0.7, list=FALSE)
+training <- iris[inTrain,]
+testing <- iris[-inTrain,]
+dim(training)
+dim(testing)
+
+#eda
+qplot(Petal.Width,Sepal.Width,colour=Species,data=training)
+
+#fit/train model
+modFit <- train(Species ~ .,method="rpart",data=training)
+print(modFit$finalModel)
+
+#predict data
+predict(modFit, newdata = testing)
+
+#confusionMatrix accuracy etc
+confusionMatrix(testing$Species, predict(modFit, newdata = testing))
